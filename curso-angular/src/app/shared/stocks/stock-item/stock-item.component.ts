b12e00d4aabd42f1376220d976f35ae57adf3508
Alejandro Coves Vives <alejandro.coves.vives@gmail.com>
2020-05-21 18:19:26 +0200

    NgClass

--- curso-angular/src/app/shared/stocks/stock-item/stock-item.component.ts
+++ curso-angular/src/app/shared/stocks/stock-item/stock-item.component.ts
@@ -9,11 +9,20 @@ import { Stock } from '../../models/stock.model';
 export class StockItemComponent implements OnInit {
 
   public stock: Stock;
+  public stockClasses: { [key: string]: any };
 
   constructor() { }
 
   ngOnInit() {
     this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+    const diff = (this.stock.price / this.stock.previousPrice) - 1;
+    const largeChange = Math.abs(diff) > 0.01;
+    this.stockClasses = {
+      positive: this.stock.isPositiveChange(),
+      negative: !this.stock.isPositiveChange(),
+      'large-change': largeChange,
+      'small-change': !largeChange
+    };
   }
 
   public toggleFavorite(event: MouseEvent) {
