63012ee29e1c5a65e53fb71352ecae8ae6e7bf26
Alejandro Coves Vives <alejandro.coves.vives@gmail.com>
2020-05-21 20:28:37 +0200

    NgFor

--- curso-angular/src/app/shared/stocks/stock-item/stock-item.component.ts
+++ curso-angular/src/app/shared/stocks/stock-item/stock-item.component.ts
@@ -8,29 +8,24 @@ import { Stock } from '../../models/stock.model';
 })
 export class StockItemComponent implements OnInit {
 
-  public stock: Stock;
-  public stockClasses: { [key: string]: any };
-  public stockStyles: { [key: string]: string };
+  public stocks: Stock[];
   constructor() { }
 
   ngOnInit() {
-    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
-    const diff = (this.stock.price / this.stock.previousPrice) - 1;
-    const largeChange = Math.abs(diff) > 0.01;
-    this.stockClasses = {
-      positive: this.stock.isPositiveChange(),
-      negative: !this.stock.isPositiveChange(),
-      'large-change': largeChange,
-      'small-change': !largeChange
-    };
-    this.stockStyles = {
-      color: this.stock.isPositiveChange() ? 'green' : 'red',
-      'font-size': largeChange ? '1.2em' : '0.8em'
-    };
+    this.stocks = [
+      new Stock('Test Stock Company', 'TSC', 85, 80),
+      new Stock('Second Stock Company', 'SSC', 10, 20),
+      new Stock('Third Stock Company', 'LSD', 15, 30),
+      new Stock('Last Stock Company', 'LSC', 876, 765)
+    ];
   }
 
-  public toggleFavorite(event: MouseEvent) {
+  public toggleFavorite(event: MouseEvent, indexSelected: number) {
     console.log('Cambiando el el estado del stock', event);
-    this.stock.favorite = !this.stock.favorite;
+    this.stocks[indexSelected].favorite = !this.stocks[indexSelected].favorite;
+  }
+
+  public trackStockByCode(index: number, stock: Stock): string {
+    return stock.code;
   }
 }
