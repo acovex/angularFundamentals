fd7c93dff2ae5d6f0a6d04f9f3b263e4826417a6
Alejandro Coves Vives <alejandro.coves.vives@gmail.com>
2020-05-23 11:14:04 +0200

    ngModel one way

--- curso-angular/src/app/shared/shared.module.ts
+++ curso-angular/src/app/shared/shared.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { StocksComponent } from './stocks/stocks.component';
 import { StockItemComponent } from './stocks/stock-item/stock-item.component';
 import { CreateStockComponent } from './stocks/create-stock/create-stock.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [StocksComponent, StockItemComponent, CreateStockComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     StocksComponent
